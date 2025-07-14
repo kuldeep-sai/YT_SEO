1e9b54a1e99e1b32cb3018abfbd3da2af5537c5c
kuldeep-sai <dhananjaynaukrigulf@gmail.com>
2025-07-14 14:01:10 +0530

    Update instagram_handler.py

--- utils/instagram_handler.py
+++ utils/instagram_handler.py
@@ -1,6 +1,8 @@
 import re
 import pandas as pd
 import time
+from io import BytesIO
+
 
 def extract_instagram_post_id(url):
     match = re.search(r"instagram.com/p/([\w-]+)/", url)
@@ -62,7 +64,14 @@ def handle_instagram_urls(file, enable_seo, client, openai_key, top_tags):
             post["seo_output"] = generate_seo_from_instagram(post, client, openai_key, top_tags)
             time.sleep(5)
         results.append(post)
-    return results
+
+    # Auto-download report for Instagram batch results
+    df = pd.DataFrame(results)
+    output = BytesIO()
+    with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+        df.to_excel(writer, index=False, sheet_name="Instagram SEO")
+    output.seek(0)
+    return results, output
 
 def get_top_instagram_hashtags(topic):
     sample = {
