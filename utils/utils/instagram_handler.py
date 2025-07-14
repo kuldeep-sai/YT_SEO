2be71e4bb0a165d20e65beef165f78afaa9657db
kuldeep-sai <dhananjaynaukrigulf@gmail.com>
2025-07-14 12:39:37 +0530

    Create instagram_handler.py

---
+++ utils/utils/instagram_handler.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+def handle_instagram_single(*args, **kwargs):
+    st.warning("🚧 Instagram single video handling not implemented yet.")
+
+def handle_instagram_batch(*args, **kwargs):
+    st.warning("🚧 Instagram batch handling not implemented yet.")
+
+def handle_instagram_urls(*args, **kwargs):
+    st.warning("🚧 Instagram URL upload handling not implemented yet.")
