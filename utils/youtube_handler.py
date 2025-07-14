af55dbb0a715e40a04079d0cb0767dfdab44c1ff
kuldeep-sai <dhananjaynaukrigulf@gmail.com>
2025-07-14 12:39:11 +0530

    Create youtube_handler.py

---
+++ utils/youtube_handler.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+def handle_youtube_batch(*args, **kwargs):
+    st.warning("🚧 YouTube batch handling not implemented yet.")
+
+def handle_youtube_single(*args, **kwargs):
+    st.warning("🚧 YouTube single handling not implemented yet.")
+
+def handle_youtube_urls(*args, **kwargs):
+    st.warning("🚧 YouTube URLs upload not implemented yet.")
